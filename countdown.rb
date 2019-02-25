c55c0d779ce4c1bf54f84e54e7aff8ca0ca274c6
Kate F <follett.katie@gmail.com>
2019-02-25 18:30:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(int)
+  int = 10
+    while int > 0
+  int -= 1
+    puts "#{int} SECOND (S) !"
+  end
+end
+
+def countdown_with_sleep(int)
 end
